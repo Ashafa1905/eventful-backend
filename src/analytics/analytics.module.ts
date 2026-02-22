86e18f7ef9d209cb1f5aa11b47c3a6cbf9982357
Ashafa Baheer <basheerashafa@gmail.com>
2026-02-22 15:22:33 +0100

    I give up

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager'; // <-- Import from cache-manager
+import { CacheModule } from '@nestjs/cache-manager';
 import * as redisStore from 'cache-manager-ioredis';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Analytics, AnalyticsSchema } from './schemas/analytics.schema';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
 
+console.log(`Redit has a port ${process.env.REDIS_PORT}`)
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Analytics.name, schema: AnalyticsSchema }]),
     CacheModule.register({
       store: redisStore,
-      host: process.env.REDIS_HOST ?? 'localhost', // Provide fallback
-      port: parseInt(process.env.REDIS_PORT ?? '6379'), // fallback to 6379
+      host: process.env.REDIS_HOST,        // Redis host
+      port: parseInt(process.env.REDIS_PORT),   // Redis port
+      auth_pass: process.env.REDIS_PASSWORD,             // Add password for cloud Redis
       ttl: 300, // 5 minutes
     }),
   ],
